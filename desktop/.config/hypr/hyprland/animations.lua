d3473d6471cf367ded3ab318f78c95e45cae1226
Joshua Kempter <joshua.kempter@m-result.de>
2026-08-15 15:14:26 +0200

    feat(desktop): configure bouncy popin window opening/closing animations

--- desktop/.config/hypr/hyprland/animations.lua
+++ desktop/.config/hypr/hyprland/animations.lua
@@ -9,14 +9,17 @@ hl.curve("specialWorkSwitch", { type = "bezier", points = { { 0.05, 0.7 }, { 0.1
 hl.curve("emphasizedAccel", { type = "bezier", points = { { 0.3, 0 }, { 0.8, 0.15 } } })
 hl.curve("emphasizedDecel", { type = "bezier", points = { { 0.05, 0.7 }, { 0.1, 1 } } })
 hl.curve("standard", { type = "bezier", points = { { 0.2, 0 }, { 0, 1 } } })
+hl.curve("bouncyDecel", { type = "bezier", points = { { 0.34, 1.56 }, { 0.64, 1 } } })
+hl.curve("bouncyAccel", { type = "bezier", points = { { 0.36, 0 }, { 0.66, -0.56 } } })
 
 -- Animation configs
 hl.animation({ leaf = "layersIn", enabled = true, speed = 5, bezier = "emphasizedDecel", style = "slide" })
 hl.animation({ leaf = "layersOut", enabled = true, speed = 4, bezier = "emphasizedAccel", style = "slide" })
 hl.animation({ leaf = "fadeLayers", enabled = true, speed = 5, bezier = "standard" })
 
-hl.animation({ leaf = "windowsIn", enabled = true, speed = 5, bezier = "emphasizedDecel" })
-hl.animation({ leaf = "windowsOut", enabled = true, speed = 3, bezier = "emphasizedAccel" })
+hl.animation({ leaf = "windowsIn", enabled = true, speed = 5, bezier = "bouncyDecel", style = "popin 75%" })
+hl.animation({ leaf = "windowsOut", enabled = true, speed = 4, bezier = "bouncyAccel", style = "popin 75%" })
+
 hl.animation({ leaf = "windowsMove", enabled = true, speed = 6, bezier = "standard" })
 hl.animation({ leaf = "workspaces", enabled = true, speed = 5, bezier = "standard" })
 
